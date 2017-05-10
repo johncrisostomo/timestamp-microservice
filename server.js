5034da47bbfacf1a7c69e1daac7298371fb0aada
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-10 11:12:52 +0800

    refactors and fixes the epoch time parsing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.get('/', (req, res) => {
   const date = Date.now();
   res.set('Content-Type', 'text/plain');
-  res.end(`This is a timestamp microservice. Try it by passing a date to the URL like so: https://johncrisostomo-timestamp.herokuapp.com/${date}`);
+  res.end(`This is a timestamp microservice. Try it by passing a date to the URL like so: https://johncrisostomo-timestamp.herokuapp.com/${Math.trunc(date/1000)}`);
 });
 
 app.get('/:input', (req, res) => {
@@ -19,22 +19,24 @@ app.get('/:input', (req, res) => {
   };
 
   if (input.match(/^[0-9]+/g)) {
-    let date = moment(new Date(Number(input)));
+    let date = moment.unix(new Date(Number(input)));
     if (date.isValid()) {
-      result.unix = date.toDate().getTime();;
-      result.natural = date.format('MMMM DD, YYYY');
+      preparePayload(result, date);
     } 
   } else {
     let date = moment(new Date(input));
 
     if (date.isValid()) {
-      result.unix = date.toDate().getTime();
-      result.natural = date.format('MMMM DD, YYYY');
+      preparePayload(result, date);
     }
   }
-
     
   res.json(result);
 });
+
+function preparePayload(result, date) {
+  result.unix = Math.round(date.toDate().getTime() / 1000);
+  result.natural = date.format('MMMM DD, YYYY');
+}
 
 app.listen(process.env.PORT || 3000);
