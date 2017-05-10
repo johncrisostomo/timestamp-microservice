5213167ac255102816af348910dc36f7da84724e
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-10 10:35:08 +0800

    implements date parsing

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var moment = require('moment');
+
+var app = express();
+
+app.get('/:input', (req, res) => {
+  const input = req.params.input;
+  let result = {
+    unix: null,
+    natural: null,
+  };
+
+  if (input.match(/^[0-9]+/g)) {
+    let date = moment(Number(input));
+    if (date.isValid()) {
+      result.unix = date.toDate().getTime();;
+      result.natural = date.format('MMMM DD, YYYY');
+    } 
+  } else {
+    let date = moment(input);
+
+    if (date.isValid()) {
+      result.unix = date.toDate().getTime();
+      result.natural = date.format('MMMM DD, YYYY');
+    }
+  }
+
+    
+  res.json(result);
+});
+
+app.listen(3000);
